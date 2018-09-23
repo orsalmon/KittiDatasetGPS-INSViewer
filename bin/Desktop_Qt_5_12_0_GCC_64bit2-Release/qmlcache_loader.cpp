17b223b657efe17650a43f0f855ce9ab400b9b66
Or Salmon <orsalmon@gmail.com>
2018-09-24 02:27:15 +0300

    [Add]: binaries for Desktop_Qt_5_12_0_GCC_64bit2

---
+++ bin/Desktop_Qt_5_12_0_GCC_64bit2-Release/qmlcache_loader.cpp
@@ -0,0 +1,62 @@
+#include <QtQml/qqmlprivate.h>
+#include <QtCore/qdir.h>
+#include <QtCore/qurl.h>
+
+static const unsigned char qt_resource_tree[] = {
+0,
+0,0,0,0,2,0,0,0,1,0,0,0,1,0,0,0,
+8,0,0,0,0,0,1,0,0,0,0};
+static const unsigned char qt_resource_names[] = {
+0,
+1,0,0,0,47,0,47,0,8,8,1,90,92,0,109,0,
+97,0,105,0,110,0,46,0,113,0,109,0,108};
+static const unsigned char qt_resource_empty_payout[] = { 0, 0, 0, 0, 0 };
+QT_BEGIN_NAMESPACE
+extern Q_CORE_EXPORT bool qRegisterResourceData(int, const unsigned char *, const unsigned char *, const unsigned char *);
+QT_END_NAMESPACE
+namespace QmlCacheGeneratedCode {
+namespace _0x5f__main_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+
+}
+namespace {
+struct Registry {
+    Registry();
+    QHash<QString, const QQmlPrivate::CachedQmlUnit*> resourcePathToCachedUnit;
+    static const QQmlPrivate::CachedQmlUnit *lookupCachedUnit(const QUrl &url);
+};
+
+Q_GLOBAL_STATIC(Registry, unitRegistry)
+
+
+Registry::Registry() {
+        resourcePathToCachedUnit.insert(QStringLiteral("/main.qml"), &QmlCacheGeneratedCode::_0x5f__main_qml::unit);
+    QQmlPrivate::RegisterQmlUnitCacheHook registration;
+    registration.version = 0;
+    registration.lookupCachedQmlUnit = &lookupCachedUnit;
+    QQmlPrivate::qmlregister(QQmlPrivate::QmlUnitCacheHookRegistration, &registration);
+QT_PREPEND_NAMESPACE(qRegisterResourceData)(/*version*/0x01, qt_resource_tree, qt_resource_names, qt_resource_empty_payout);
+}
+const QQmlPrivate::CachedQmlUnit *Registry::lookupCachedUnit(const QUrl &url) {
+    if (url.scheme() != QLatin1String("qrc"))
+        return nullptr;
+    QString resourcePath = QDir::cleanPath(url.path());
+    if (resourcePath.isEmpty())
+        return nullptr;
+    if (!resourcePath.startsWith(QLatin1Char('/')))
+        resourcePath.prepend(QLatin1Char('/'));
+    return unitRegistry()->resourcePathToCachedUnit.value(resourcePath, nullptr);
+}
+}
+int QT_MANGLE_NAMESPACE(qInitResources_qml)() {
+    ::unitRegistry();
+    return 1;
+}
+Q_CONSTRUCTOR_FUNCTION(QT_MANGLE_NAMESPACE(qInitResources_qml))
+int QT_MANGLE_NAMESPACE(qCleanupResources_qml)() {
+    return 1;
+}
